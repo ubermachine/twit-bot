565f72bde2ae12c0e4c2245d2ef5d4ff8439eb80
ubermachine <shivamkatoch@outlook.com>
2020-04-26 18:36:06 +0530

    make it bettetyufgrff252u525dsmsff]c'

--- app3.js
+++ app3.js
@@ -6,7 +6,7 @@ var config = require("./congig.js");
 
 var cron = require('node-cron');
  
-cron.schedule('*/5 * * * *', () => {
+cron.schedule('*/3 * * * *', () => {
   console.log('running a task every minute');
 
 
