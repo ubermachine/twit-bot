e69c6b14bf9452c8fb6160bf3aa2563de4f84e82
ubermachine <shivamkatoch@outlook.com>
2020-04-26 17:41:48 +0530

    make it bettetyufgrff252u525dsms'

--- app3.js
+++ app3.js
@@ -175,3 +175,15 @@ T.get('search/tweets', params, function(err, data) {
   });server.listen(PORT, () => {
     console.log(`Server running on ${PORT}/`);
   });
+
+
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`https://twitbo.herokuapp.com/`);
+}, 1200000);
